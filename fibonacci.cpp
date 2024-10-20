c6b4169a62bf0f6aab75c8de6d5498c1d7a53894
hariniSruthi301 127 <nsharini127@gmail.com>
2024-10-20 14:06:02 +0000

    Hello

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -8,6 +8,7 @@ int fibonacci(int n) {
 }
 
 int main(int argc, char* argv[]) {
+    std::cout<<"Hello";
     if (argc < 2) {
         std::cerr << "Usage: " << argv[0] << " <n>" << std::endl;
         return 1; // Exit with error code
